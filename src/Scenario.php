e94f9ce3cba6054bf645dd4007a222b73c6fa775
Don Kidd <kidddw@miamioh.edu>
2018-01-26 15:31:40 -0500

    Creation of Git Classes Repo, class files extracted from CSVFormatter Repo to create this initial library

---
+++ src/Scenario.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace miamioh\Behat\Classes;
+use Behat\Gherkin\Node\ScenarioNode;
+
+class Scenario extends ScenarioNode
+{
+
+  public function __construct(string $title, array $tags = [], array $steps = [], $keyword = null, $line = null) {
+    parent::__construct($title,$tags,$steps,$keyword,$line);
+
+  }
+
+  public function __toString(): string
+  {
+      return $this->getTitle();
+  }
+  public function asArray(): array
+  {
+    return array('Name'=>$this->getTitle(),'Tags'=>$this->getTagsAsString());
+  }
+
+  public function getTagsAsString() {
+    return implode(',',$this->getTags());
+  }
+
+}
